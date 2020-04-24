c6f9eef12da5d3adc48cd8d37eaab984fe6009a3
akashvijay98 <31077544+akashvijay98@users.noreply.github.com>
2020-04-24 13:11:40 +0530

    Add files via upload

---
+++ list_to_dictionary.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[10]:
+
+
+d=dict()
+a=[]
+b=[]
+
+for i in range(3):
+    k=int(input("enter key vlue"))
+    a.append(k)
+    v=int(input("enter vlue"))
+    b.append(v)
+
+for i in range(len(a)):
+    d[a[i]]=b[i]
+
+
+print(d)
+
+
+# ## 
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
