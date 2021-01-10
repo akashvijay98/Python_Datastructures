c17f94dd8b35a634f4d22d7c799b3b8f3c7ac23b
akashvijay98 <31077544+akashvijay98@users.noreply.github.com>
2021-01-10 19:44:10 +0530

    Update linkedlist.py

--- linkedlist.py
+++ linkedlist.py
@@ -1,73 +1,119 @@
-#!/usr/bin/env python
-# coding: utf-8
+class Node:
+    def __init__(self,val,next=None):
+        self.val=val
+        self.next=next
+        
 
-# In[1]:
+class MyLinkedList:
 
-
-class node:
-    def __init__(self,data):
-        self.data=data
-        self.next=None
-class linkedlist:
-    
-    def __init__(self):
+    def __init__(self):        
         self.head=None
+        self.len=0
+
+        
     
-    def insert(self,value):
-        newnode=node(value)
-        if self.head==None:
-            self.head=newnode
-            return
+    def get(self, index: int) -> int:
+        
+        if index>=self.len:
+            return -1
+        
+        
+        
         l=self.head
-        while(l.next):
-            l=l.next
-        l.next=newnode
-    
-    def printv(self):
-        a=[]
-        printval=self.head
-        while(printval is not None):
-            a.append(printval.data)
-            print(printval.data)
-            printval=printval.next
-            
-        return a
-    
-    def cycle(self):
-        p1=p2=self.head
+        j=0
+        while(j<=index):
+            if(j==index):
+                #print("element="+str(l.val))
+                return l.val
+            else:
+                j+=1
+                l=l.next
         
-        while(p2!=None and p2.next!=None):
-            p1=p1.next
-            p2=p2.next.next
+        
+        
+
+    def addAtHead(self, val: int) -> None:
+        if self.head==None:
+            self.head=Node(val)
+            self.len+=1
+        else:
+            n=Node(val)
             
-            if(p1==p2):
-                return True
+            n.next=self.head
+            self.head=n
+            self.len+=1
             
+
+    def addAtTail(self, val: int) -> None:
+        n=Node(val)
+        p=self.head
+        
+        if self.head==None:
+            self.head=Node(val)
+            self.len+=1
+            return 
+        
+        while(p.next!=None):
+            p=p.next
+        p.next=n
+        self.len+=1
+        
+
+    def addAtIndex(self, index: int, val: int) -> None:
+        if index>self.len:
+            return -1
+        if index==0:
+            self.addAtHead(val)
             
+        i=0
+        q=self.head
+        p=self.head
+        
+       
+        if index>self.len:
+            return -1
+        while(i<=self.len):
+            if i==index:
+                n=Node(val)
+                q.next=n
+                n.next=p
+                self.len+=1
+                return
+                
+                
+                
+                
+            else:
+                i+=1
+                q=p
+                p=p.next
         
             
-        return False
- 
+
+    def deleteAtIndex(self, index: int) -> None:
+        i=0
+        p=self.head
+        q=self.head
         
-            
-
-    
+        if index>=self.len:
+            return -1
         
-l=linkedlist()
-l.head=node(0)
-l.insert(1)
-l.insert(2)
-l.insert(3)
-l.insert(4)
-l.printv()
-
-l.cycle()
-
-
-
-
-# In[ ]:
-
-
+        if index==0:
+            q=p.next
+            self.head=q
+            return 
+        
+        while(i<=index):
+            if(i==index):
+                q.next=p.next
+                
+                self.len-=1
+                break
+                
+            else:
+                i+=1
+                q=p
+                p=p.next
+                
 
 
